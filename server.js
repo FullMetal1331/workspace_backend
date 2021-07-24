87efd6c57e8092a3a4c54be17e3ae4a59e950455
FullMetal1331 <sakshamtewari8@gmail.com>
2021-07-24 16:18:50 +0530

    updating database server link

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = require('knex')({
   client: 'pg',
   version: '7.2',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '123',
-    database : 'schedule'
+    host : process.env.DATABASE_URL,
+    ssl : true,
   }
 });
 
